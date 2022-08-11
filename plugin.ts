f243d6278bcb5de720f5ee776be401a63980fe16
react-spy <996251389@qq.com>
2022-08-11 20:29:38 +0800

    first commit

---
+++ plugin.ts
@@ -0,0 +1,5 @@
+import type { IApi } from 'umi';
+
+export default (api: IApi) => {
+    // api.addHTMLHeadScripts(() => [{ src: "https://cdn.tiny.cloud/1/fy54jxb8dk9r04hgcjl71bdk4iijglbjskq6e0yk8i1lces1/tinymce/6/tinymce.min.js", type: "text/javascript" }]);
+};
